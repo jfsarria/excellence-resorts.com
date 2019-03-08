b02e69081bef84f0ef98f99a40166ea9a5862254
Juan F Sarria <juan@townsquaredigital.com>
2019-03-08 14:11:24 -0500

    Fixing GTM

--- er/confirmation.php
+++ er/confirmation.php
@@ -68,8 +68,6 @@
             }
         </style>
 
-	</head>
-	<body>
         <script>
             dataLayer = [{
                 'Page' : 'Thank you page',
@@ -92,7 +90,21 @@
             }];
         </script>
 
-        <? include "../GTM.php"; ?>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TTL2Q6');</script>
+        <!-- End Google Tag Manager -->   
+
+	</head>
+	<body>
+
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TTL2Q6"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->  
         
 		<div id="wrapper">
 			<? include "top.php"; ?>
